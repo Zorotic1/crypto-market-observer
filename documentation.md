dca7a4e083ac783ab630b36042fa74f89b7e5fd6
Khor Zhan Ming <jc081380@gmail.com>
2026-02-02 15:04:15 +0800

    initial prototype of the market observer

--- documentation.md
+++ documentation.md
@@ -4,3 +4,9 @@
 * **rationale:**
     * `.env`: to prevent api key leakage.
     * `__pycache__`: prevent binary incompatibility between machines and potential merge conflicts.
+
+
+
+## To-Do List
+* add functionality to search with name instead of id for api
+* further error checking with api (guardrails for character limit, character type, etc)
--- main.py
+++ main.py
@@ -2,9 +2,26 @@
 import requests
 import py_code.config as config
 import py_code.database as database
+import time
+
 
 #details of coin
 def fetch_price(token_name):
+
+#for searching by name, implement later, use .replace() instead.
+    #while " " in token_name:
+        #temp_name = ""
+        #c = 0
+        #for i in token_name:
+            #if i == " ":
+                #temp_name = temp_name + ('%20')
+            #else:
+                #temp_name = temp_name + i
+            #c = c + 1
+            #if c == len(token_name):
+                #token_name = temp_name
+                #break
+
 
     params = {
         'ids': token_name,
@@ -22,37 +39,45 @@ def fetch_price(token_name):
     while state != True:
         try:
             res = requests.get(config.API_URL, params=params, headers=headers)
-            status = res.raise_for_status()
-        except status == 400:
-            print('(400, Bad Request)')
-        except status == 401:
-            print('(401, Unauthorized)')
-        except status == 403:
-            print('(403, Forbidden)')
-        except status == 429:
-            print('(429, Too many requests)')
-        except status == 500:
-            print('(500, Internal Server Error)')
-        except status == 503:
-            print('(503, Service Unavailable)')
-        except status == 1020:
-            print('(1020, Access Denied, violation of CDN firewaLL rule)')
-        except status == 10002:
-            print('(10005, Missing API Key)')
-        except status == 10010:
-            print('(10010, Invalid API Key, change from demo to pro)')
-        except status == 1020:
-            print('(10011, Invalid API Key, change from pro to demo)')
-        except status == 10005:
-            print('(10005, no access to endpoint, need upgrade for access)')
-        else:
+            res.raise_for_status()
             state = True
-   
-    return res.json()
+            return res.json()
+        except requests.exceptions.HTTPError as e:
+            code = e.response.status_code
+            if code == 429:
+                print('(429, Too many requests)')
+                time.sleep(60)
+            elif code >= 500 or code == 400:
+                print('Error Code: ', code, ', terminating.')
+                break
+            else:
+                print('Error Code:', code)
+                time.sleep(5)
+        except res == None:
+            return None
+    return None
 
+#main
 if __name__ == '__main__':
     database.init_db()
+    #token name input
     token_name = input((str('please enter a valid token name: ')))
+    while token_name.isspace():
+            token_name = input((str('please enter a valid token name: ')))
+
     results = fetch_price(token_name)
+
+    #retry if there are no results or error happened
+    if results == None:
+        choice = list('Y', 'N', "y", 'n')
+        decision = None
+        while decision not in choice:
+            decision = str(input('retry? Y/N'))
+            if decision == 'Y' or 'y':
+                token_name = input((str('please enter a valid token name: ')))
+                results = fetch_price(token_name)
+            else:
+                break
+    
     print('fetched: ', results)
     database.save_to_db(results, token_name)
